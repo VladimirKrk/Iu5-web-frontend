1baaa73bf93bb8e538cc651c783badef127e3e31
VladimirKrk <vlkgroupyt@gmail.com>
2025-11-12 13:25:44 +0300

    feat: configure gh pages and pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/Iu5-web-frontend/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Исторические мастерские',
+        short_name: 'Мастерские',
+        description: 'Приложение для расчета производственных мощностей',
+        theme_color: '#ffffff',
+        icons: [
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
   server: {
     proxy: {
       '/api': {
